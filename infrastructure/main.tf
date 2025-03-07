b4854e5e88414d8d62b8a69d4778de94c2b387b8
Elikariuki <ngugidoc4000@gmail.com>
2025-03-06 22:05:02 -0500

    Add properly formatted Terraform script

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,12 +18,13 @@ resource "azurerm_storage_account" "example" {
   name                     = "kari0117-store" # Storage account name must be globally unique
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"  # Choose between Standard and Premium
-  account_replication_type = "LRS"       # LRS = Locally redundant storage
+  account_tier             = "Standard" # Choose between Standard and Premium
+  account_replication_type = "LRS"      # LRS = Locally redundant storage
 }
 
-# Output the storage account primary access keyoutput "storage_account_primary_access_key" {
+# Output the storage account primary access key
+output "storage_account_primary_access_key" {
   value       = azurerm_storage_account.example.primary_access_key
   description = "The primary access key for the storage account"
-  sensitive   = tru # Marks this output as sensitive
+  sensitive   = true # Marks this output as sensitive
 }
